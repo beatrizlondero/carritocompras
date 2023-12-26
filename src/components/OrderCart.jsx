d4d833f1465e4cd69cf42191e780e5fb68558430
Beatriz Londero <beatriz04016@gmail.com>
2023-12-26 18:21:17 -0300

    con arreglos en la lista de compras

--- src/components/OrderCart.jsx
+++ src/components/OrderCart.jsx
@@ -1,84 +1,99 @@
-import { Typography, List, ListItem, ListItemText, Button } from '@mui/material';
-import { orange } from '@mui/material/colors';
-import CheckoutModal from './CheckoutModal'
-
 import React, { useState } from 'react';
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+} from '@mui/material';
+import { orange } from '@mui/material/colors';
+import CheckoutModal from './CheckoutModal';
 
 const OrderCart = ({ cart, removeFromCart, calculateTotal, clearCart, sortOrder }) => {
   const [isCheckoutOpen, setIsCheckoutOpen] = useState(false);
 
-    // Function para ordenar por precio
-    const comparePrices = (a, b) => {
-      if (sortOrder === 'asc') {
-        return a.price - b.price;
-      } else if (sortOrder === 'desc') {
-        return b.price - a.price;
-      } else {
-        return 0; 
-      }
-    };
+  // Function para ordenar por precio
+  const comparePrices = (a, b) => {
+    if (sortOrder === 'asc') {
+      return a.price - b.price;
+    } else if (sortOrder === 'desc') {
+      return b.price - a.price;
+    } else {
+      return 0;
+    }
+  };
 
-    // Function to open the checkout modal
   const handleOpenCheckout = () => {
     setIsCheckoutOpen(true);
   };
 
-  // Function to close the checkout modal
   const handleCloseCheckout = () => {
     setIsCheckoutOpen(false);
   };
-  
-    // Ordenar los productos segun los precios
-    const sortedCart = [...cart].sort(comparePrices);
-  
-    return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', backgroundColor: '#ffebb3' }}>
-        <Typography variant="h3" gutterBottom color={orange[900]}>
-          Carrito de Compra
-        </Typography>
-        <List>
-          {sortedCart.map((product, index) => (
-            <ul style={{ fontSize: '50px', width: '800px', marginBottom: '15px', color: '#5a3e2b' }}>
-              <div>
-                <li style={{ fontSize: '2rem', marginRight: '10px' }}>
-                  {product.title}
-                  <span style={{ fontSize: '2rem', marginRight: '10px', marginLeft: '10px', color: 'green' }}>
-                    {`$${product.price}`}
-                  </span>
-                  <Button variant="outlined" color="warning" size="large" onClick={() => removeFromCart(index)}>
-                    Remove
-                  </Button>
-                </li>
-              </div>
-            </ul>
-          ))}
-        </List>
-        <Typography variant="h3" gutterBottom>
-          Total: ${calculateTotal().toFixed(2)}
-        </Typography>
-        <div >
 
-          <Button variant="contained" color="warning" size="large" padding="10px" onClick={clearCart}>
-            Vaciar el Carro
-          </Button>
-        </div>
-        <div className='buttons'>
+  // crea un map con las cantidades de los productos
+  const productQuantities = {};
 
-          <Button variant="contained" color="success" size="large" padding="10px" onClick={handleOpenCheckout}>
-            Ir al Pago
-          </Button>
-            {isCheckoutOpen && (
-            <CheckoutModal
-              cart={cart}
-              handleCloseCheckout={handleCloseCheckout}
-              totalAmount={calculateTotal().toFixed(2)}
-              clearCart={clearCart}
-            />
-           )}
-         </div>
+  // actualiza los productos y cantidades en el map
+  cart.forEach((product) => {
+    const productId = product.id;
+    if (productId in productQuantities) {
+      productQuantities[productId].quantity++;
+    } else {
+      productQuantities[productId] = { product, quantity: 1 };
+    }
+  });
 
+  // Ordenar los productos segun los precios
+  const sortedCart = Object.values(productQuantities).map(({ product }) => product).sort(comparePrices);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', backgroundColor: '#ffebb3' }}>
+      <Typography variant="h3" gutterBottom color={orange[900]}>
+        Carrito de Compra
+      </Typography>
+      <List>
+        {sortedCart.map((product) => (
+          <ul key={product.id} style={{ fontSize: '50px', width: '800px', marginBottom: '15px', color: '#5a3e2b' }}>
+            <div>
+              <li style={{ fontSize: '2rem', marginRight: '10px' }}>
+                {product.title}
+                <span style={{ fontSize: '2rem', marginRight: '10px', marginLeft: '10px', color: 'green' }}>
+                  {`$${product.price} x ${productQuantities[product.id].quantity}`}
+                </span>
+                <Button variant="outlined" color="warning" size="large" onClick={() => removeFromCart(cart.indexOf(product))}>
+                  Remove
+                </Button>
+              </li>
+            </div>
+          </ul>
+        ))}
+      </List>
+      <Typography variant="h3" gutterBottom>
+        Total: ${calculateTotal().toFixed(2)}
+      </Typography>
+      <div>
+        <Button variant="contained" color="warning" size="large" padding="10px" onClick={clearCart}>
+          Vaciar el Carro
+        </Button>
       </div>
-    );
-  };
-  
-  export default OrderCart;
+      <div className='buttons'>
+        <Button variant="contained" color="success" size="large" padding="10px" onClick={handleOpenCheckout}>
+          Ir al Pago
+        </Button>
+        {isCheckoutOpen && (
+          <CheckoutModal
+            cart={cart}
+            handleCloseCheckout={handleCloseCheckout}
+            totalAmount={calculateTotal().toFixed(2)}
+            clearCart={clearCart}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OrderCart;
+
+
