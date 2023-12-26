0aefa04c5b1b2439771520e08e28c51ce743f1e0
Beatriz Londero <beatriz04016@gmail.com>
2023-12-26 18:35:29 -0300

    con validacion de tarjeta

--- src/components/CheckoutModal.jsx
+++ src/components/CheckoutModal.jsx
@@ -1,12 +1,49 @@
-// 
-import React, { useState } from 'react';
-import { Modal, Fade, Typography, TextField, Button, List, ListItem, ListItemText, FormControl, RadioGroup, Radio, FormControlLabel } from '@mui/material';
 
-const CheckoutModal = ({ cart, handleCloseCheckout, totalAmount, clearCart}) => {
+import React, { useState } from 'react';
+import {
+  Modal,
+  Fade,
+  Typography,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  FormControl,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from '@mui/material';
+
+const CreditCardInput = ({ cardNumber, handleCardNumberChange }) => {
+    const formatCardNumber = (value) => {
+      const numericValue = value.replace(/\D/g, '');
+      const formattedValue = numericValue.replace(/(\d{4})/g, '$1-');
+      const finalValue = formattedValue.slice(0, 19); // Limit to 16 digits
+      return finalValue;
+    };
+  
+    return (
+      <TextField
+        label="Card Number"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={formatCardNumber(cardNumber)}
+        onChange={(e) => handleCardNumberChange(e)}
+        inputProps={{
+          maxLength: 19,
+          pattern: '[0-9]*',
+        }}
+      />
+    );
+  };
+  
+
+const CheckoutModal = ({ cart, handleCloseCheckout, totalAmount, clearCart }) => {
   const [paymentMethod, setPaymentMethod] = React.useState('cash');
   const [cardNumber, setCardNumber] = useState('');
-  //const importeTotal = parseFloat(totalAmount);
-    console.log(totalAmount);
+
   const handlePaymentMethodChange = (event) => {
     setPaymentMethod(event.target.value);
   };
@@ -15,139 +52,70 @@ const CheckoutModal = ({ cart, handleCloseCheckout, totalAmount, clearCart}) => 
     setCardNumber(event.target.value);
   };
 
-  
-    const handleConfirmPurchase = () => {
-      // proceso simulado de pago
-      if (paymentMethod === 'card' && cardNumber.trim() === '') {
-        //error si esta en blanco
-        alert('Please enter a valid card number.');
-      } else {
-        // simula una compra exitosa
-        alert('La compra fue exitosa, gracias por elegirnos!.');
-        clearCart();
-        
-        handleCloseCheckout();
-      }
-    };
-  
-    const handleBackToStore = () => {
+  const handleConfirmPurchase = () => {
+    if (paymentMethod === 'card' && cardNumber.trim() === '') {
+      alert('Por favor ingrese un numero de tarjeta valido.');
+    } else {
+      alert('La compra fue exitosa, gracias por elegirnos!.');
+      clearCart();
       handleCloseCheckout();
-    };
-  
-    return (
-        <Modal
-        open={true} // se asegura que este abierto
-        onClose={handleCloseCheckout}
-        closeAfterTransition
-    >
-        <Fade in={true}>
-            <div style={{ backgroundColor: 'white', padding: 20, maxWidth: 400, margin: 'auto',fontSize: '16px' }}>
-                <Typography variant="h5">Seccion de Pago</Typography>
-
-                {/* Display total amount */}
-              <Typography variant="subtitle1">Importe Total: ${totalAmount}</Typography>
-
-                {/* Display items being purchased */}
-                <List>
-                {cart.map((product, index) => (
-                    <ListItem key={index}>
-                    <ListItemText primary={product.title} secondary={`$${product.price.toFixed(2)}`} />
-                    </ListItem>
-                ))}
-                </List>
-
-          
-                <FormControl component="fieldset">
-               <RadioGroup
-                    row
-                    aria-label="payment-method"
-                    name="payment-method"
-                    value={paymentMethod}
-                    onChange={handlePaymentMethodChange}
-                >
-                <FormControlLabel value="cash" control={<Radio />} label="Cash" />
-                <FormControlLabel value="card" control={<Radio />} label="Debit Card" />
-              </RadioGroup>
-            </FormControl>
-  
-            {/* Card input (visible only when paying with a card) */}
-            {paymentMethod === 'card' && (
-              <TextField
-                label="Card Number"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={cardNumber}
-                onChange={handleCardNumberChange}
-              />
-            )}
-  
-                <div className='buttons'>
-
-                    <Button variant="contained" color="error" onClick={handleConfirmPurchase}>
-                        Confirmar Compra
-                    </Button>
-                </div>
-                <div className='buttons'>
-                    <Button variant="outlined" color="secondary" onClick={handleBackToStore}>
-                        Volver a la Tienda
-                    </Button>
-
-                </div>
-            </div>
-        </Fade>
-    </Modal>
-    //   <Modal
-    //     open={true}
-    //     onClose={handleCloseCheckout}
-    //     closeAfterTransition
-    //     BackdropComponent={Backdrop}
-    //     BackdropProps={{ timeout: 500 }}
-    //   >
-    //     <Fade in={true}>
-    //       <div>
-    //         <Typography variant="h5" gutterBottom>
-    //           Checkout
-    //         </Typography>
-  
-    //         {/* Payment method selection */}
-    //         <FormControl component="fieldset">
-    //           <RadioGroup
-    //             row
-    //             aria-label="payment-method"
-    //             name="payment-method"
-    //             value={paymentMethod}
-    //             onChange={handlePaymentMethodChange}
-    //           >
-    //             <FormControlLabel value="cash" control={<Radio />} label="Cash" />
-    //             <FormControlLabel value="card" control={<Radio />} label="Debit Card" />
-    //           </RadioGroup>
-    //         </FormControl>
-  
-    //         {/* Card input (visible only when paying with a card) */}
-    //         {paymentMethod === 'card' && (
-    //           <TextField
-    //             label="Card Number"
-    //             variant="outlined"
-    //             fullWidth
-    //             margin="normal"
-    //             value={cardNumber}
-    //             onChange={handleCardNumberChange}
-    //           />
-    //         )}
-  
-    //         <div>
-    //           <Button variant="contained" color="primary" onClick={handleConfirmPurchase}>
-    //             Confirm Purchase
-    //           </Button>
-    //           <Button variant="outlined" color="secondary" onClick={handleBackToStore}>
-    //             Back to Store
-    //           </Button>
-    //         </div>
-    //       </div>
-    //     </Fade>
-    //   </Modal>
-    );
+    }
   };
-  
-  export default CheckoutModal;
+
+  const handleBackToStore = () => {
+    handleCloseCheckout();
+  };
+
+  return (
+    <Modal open={true} onClose={handleCloseCheckout} closeAfterTransition>
+      <Fade in={true}>
+        <div style={{ backgroundColor: 'white', padding: 20, maxWidth: 400, margin: 'auto', fontSize: '16px' }}>
+          <Typography variant="h5">Seccion de Pago</Typography>
+
+          {/* Muestra el importe total */}
+          <Typography variant="subtitle1">Importe Total: ${totalAmount}</Typography>
+
+          {/* Muestra los items de las compras */}
+          <List>
+            {cart.map((product, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={product.title} secondary={`$${product.price.toFixed(2)}`} />
+              </ListItem>
+            ))}
+          </List>
+
+          <FormControl component="fieldset">
+            <RadioGroup
+              row
+              aria-label="payment-method"
+              name="payment-method"
+              value={paymentMethod}
+              onChange={handlePaymentMethodChange}
+            >
+              <FormControlLabel value="cash" control={<Radio />} label="Efectivo" />
+              <FormControlLabel value="card" control={<Radio />} label="Tarjeta de Debito" />
+            </RadioGroup>
+          </FormControl>
+
+          {/* Card input (visible solo cuando se elige tarjeta) */}
+          {paymentMethod === 'card' && (
+            <CreditCardInput cardNumber={cardNumber} handleCardNumberChange={handleCardNumberChange} />
+          )}
+
+          <div className="buttons">
+            <Button variant="contained" color="error" onClick={handleConfirmPurchase}>
+              Confirmar Compra
+            </Button>
+          </div>
+          <div className="buttons">
+            <Button variant="outlined" color="secondary" onClick={handleBackToStore}>
+              Volver a la Tienda
+            </Button>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default CheckoutModal;
